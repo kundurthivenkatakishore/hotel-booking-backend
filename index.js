5a28d7983c64bd7a38b7eefa4e125ebbe4688c7d
kundurthivenkatakishore <kundurthivenkatakishore@gmail.com>
2022-07-16 09:17:40 +0530

    intial commit

---
+++ index.js
@@ -0,0 +1,38 @@
+import express from "express";
+import dotenv from 'dotenv';
+import mongoose from "mongoose";
+import authRoute from './routes/auth.js';
+import usersRoute from './routes/users.js';
+import hotelsRoute from './routes/hotels.js';
+import roomsRoute from './routes/rooms.js';
+import cookieParser from "cookie-parser";
+import cors from 'cors';
+
+const app=express();
+dotenv.config();
+
+const connect=async()=>{
+    try{
+        await mongoose.connect(process.env.MONGO);
+        console.log("Connected to MongoDB")
+    } catch(error){
+        throw error;
+    }
+}
+
+app.use(cors())
+app.use(cookieParser());
+app.use(express.json());
+
+app.use("/api/auth",authRoute);
+app.use("/api/hotels",hotelsRoute);
+app.use("/api/users",usersRoute);
+app.use("/api/rooms",roomsRoute);
+
+
+app.listen(8800,()=>{
+    connect()
+    console.log("Connected to backend")
+})
+
+//72kOuMKQIAVUW1nQ
